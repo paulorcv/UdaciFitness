4386326bc954f3b0375a8afce0a3f7f54a3d6af8
Paulo <paulorcv@gmail.com>
2019-04-06 18:52:35 -0300

    statusBar

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
-
+import { Constants } from 'expo';
 import TabNav from './components/TabNav';
+import { purple } from './utils/colors';
+
+
+function UdaciStatusBar( { backgroundColor, ...props}){
+  return(
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} { ...props } />
+    </View>
+  )
+}
+
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1}}>
+          <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
           <TabNav />
         </View>
       </Provider>

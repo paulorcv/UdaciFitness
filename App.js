2b3f2b7c601f2a2fd759880bbcd3d8b5281a4548
Paulo <paulorcv@gmail.com>
2019-04-06 18:01:32 -0300

    react-navigation v3

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import { View } from 'react-native'
-import AddEntry from './components/AddEntry'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import reducer from './reducers'
-import History from './components/History'
+import React from 'react';
+import { View } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+
+import TabNav from './components/TabNav';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View style={{flex: 1}}>
-          <History />
+        <View style={{ flex: 1}}>
+          <TabNav />
         </View>
       </Provider>
-    )
+    );
   }
 }
